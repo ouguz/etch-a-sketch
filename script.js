407b9b34ba3a5526be3cb9fadfb9b77728801b07
OnurUguz <onur_uguz@hotmail.com>
2023-07-08 08:23:14 +0300

    feature: add color randomizer button

--- script.js
+++ script.js
@@ -10,16 +10,54 @@ for (let i = 0; i < a*a; i++) {
     container.appendChild(square);
 };
 
+const green = document.querySelector('#green');
+green.addEventListener('click', greenColor);
+function greenColor () {
 container.addEventListener('mouseover', (event) => {
     event.target.setAttribute('class', 'makeItGreen');
 });
-
-let children = container.children;
-console.log(children);
+};
 
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', () => {
-    square.removeAttribute('class');
+    let allChildren = container.children;
+    for (child of allChildren) {
+        // child.classList.remove('makeItGreen');
+        // child.classList.remove('makeItBlack');
+        // child.classList.remove('makeItRed');
+        // child.classList.remove('makeItWhite');
+        // child.classList.add =('makeItYellow');
+        // container.style.backgroundColor = 'yellow';
+        child.style.backgroundColor = 'yellow';
+    };
 });
 
+// container.addEventListener('mouseover', (event) => {
+//     event.target.classList.add('makeItWhite');
+//     event.target.style.backgroundColor = 'white';
+// });
+const rainbow = document.querySelector('#rainbow');
+rainbow.addEventListener('click', randomColor);
+    
+function randomColor () {
+    container.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+    });
+};
+
+    
+
+function randomNumber () {
+    let randomize = Math.floor(Math.random() * 256);
+    return randomize
+};
+
+const black = document.querySelector('#black');
+black.addEventListener('click', blackColor);
+function blackColor () {
+    container.addEventListener('mouseover', (event) => {
+        // event.target.setAttribute('class', 'makeItBlack');
+        event.target.style.backgroundColor = 'black';
+    });
+};
 
