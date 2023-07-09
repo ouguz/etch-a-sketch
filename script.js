54cb62be18548bd884e4d36c22f65f825126040a
OnurUguz <onur_uguz@hotmail.com>
2023-07-09 07:56:03 +0300

    feature: button replacements and styling

--- script.js
+++ script.js
@@ -5,54 +5,65 @@ for (let i = 0; i < 16*16; i++) {
     square.classList.add('squares');
     square.style.height = `${100/16}%`;
     square.style.width = `${100/16}%`;
-    square.style.border = "1px solid black";
+    // square.style.border = "1px solid black";
     container.appendChild(square);
 };
-// let a = prompt('hwat', 4);
 
-const newSize = document.querySelector('#newSize');
-newSize.addEventListener('click', () =>{
+const slider = document.querySelector('#myRange');
+const sliderIndicator = document.querySelector('#demo');
+sliderIndicator.textContent = '16 x 16';
+slider.addEventListener('change', () =>{
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     };
-    let a = prompt('kk');
+    let a = slider.value;
+    sliderIndicator.textContent = `${slider.value} x ${slider.value}`;
     for (let i = 0; i < a*a; i++) {
         let square = document.createElement('div');
         square.style.height = `${100/a}%`;
         square.style.width = `${100/a}%`;
-        square.style.border = "1px solid black";
+        // square.style.border = "1px solid black";
         container.appendChild(square);
     };
 });
 
-
-
-const green = document.querySelector('#green');
-green.addEventListener('click', greenColor);
-function greenColor () {
 container.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = 'green';
+    event.target.style.backgroundColor = `royalblue`;
 });
+
+const black = document.querySelector('#black');
+black.addEventListener('click', blackColor);
+function blackColor () {
+    container.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'black';
+    });
+};
+
+const colorPicker = document.querySelector('#colorPicker');
+colorPicker.addEventListener('click', pickYourColor);
+function pickYourColor () {
+container.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = colorPicker.value;
+});
+};
+
+const eraser = document.querySelector('#eraser');
+eraser.addEventListener('click', eraseIt);
+function eraseIt () {
+    container.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'white';
+    });
 };
 
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', () => {
     let allChildren = container.children;
     for (child of allChildren) {
-        // child.classList.remove('makeItGreen');
-        // child.classList.remove('makeItBlack');
-        // child.classList.remove('makeItRed');
-        // child.classList.remove('makeItWhite');
-        // child.classList.add =('makeItYellow');
-        // container.style.backgroundColor = 'yellow';
         child.style.backgroundColor = 'white';
     };
 });
 
-// container.addEventListener('mouseover', (event) => {
-//     event.target.classList.add('makeItWhite');
-//     event.target.style.backgroundColor = 'white';
-// });
+
 const rainbow = document.querySelector('#rainbow');
 rainbow.addEventListener('click', randomColor);
     
@@ -61,24 +72,8 @@ function randomColor () {
         event.target.style.backgroundColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
     });
 };
-
-container.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
-});
-
     
-
 function randomNumber () {
     let randomize = Math.floor(Math.random() * 256);
     return randomize
 };
-
-const black = document.querySelector('#black');
-black.addEventListener('click', blackColor);
-function blackColor () {
-    container.addEventListener('mouseover', (event) => {
-        // event.target.setAttribute('class', 'makeItBlack');
-        event.target.style.backgroundColor = 'black';
-    });
-};
-
